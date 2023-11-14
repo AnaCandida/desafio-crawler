fd19e62aa25656b2db7b0e3d856adeb1403b49e1
AnaCandida <ana.candida@instruct.com.br>
2023-11-14 13:00:23 -0300

    feat(pipeline): implement data storage pipeline
    Introduces `SavingToPostgresPipeline` for saving scraped data to a PostgreSQL database.
    Utilizes batch insert for improved performance and employs the `ON CONFLICT` clause to handle conflicts based on the `rank` field.
    In case of a conflict, it updates the existing record with the new values.

--- imdb_crawler/imdb_crawler/pipelines.py
+++ imdb_crawler/imdb_crawler/pipelines.py
@@ -1,13 +1,89 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+import logging
+import os
 
-
-# useful for handling different item types with a single interface
+import psycopg2
 from itemadapter import ItemAdapter
+from psycopg2 import DataError, IntegrityError, OperationalError
 
 
 class ImdbCrawlerPipeline:
     def process_item(self, item, spider):
         return item
+
+
+class SavingToPostgresPipeline(object):
+    def __init__(self):
+        self.items_to_store = []
+
+    def create_connection(self):
+        logging.info("Connecting to the database")
+
+        self.connection = psycopg2.connect(
+            host=os.getenv("DB_HOST"),
+            database=os.getenv("DB_NAME"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+        )
+
+        self.curr = self.connection.cursor()
+
+    def close_connection(self):
+        if self.connection:
+            self.connection.commit()
+            self.connection.close()
+            logging.info("Database connection finished")
+
+    def process_item(self, item, spider):
+        try:
+            self.items_to_store.append(item)
+        except Exception as e:
+            spider.logger.error(f"Error processing item: {e}")
+
+        return item
+
+    def store_db(self, items, spider):
+        try:
+            self.create_connection()
+
+            values = [
+                (
+                    ItemAdapter(item).get("rank"),
+                    ItemAdapter(item).get("title"),
+                    ItemAdapter(item).get("stars"),
+                    ItemAdapter(item).get("year"),
+                    ItemAdapter(item).get("duration"),
+                )
+                for item in items
+            ]
+
+            self.curr.executemany(
+                """INSERT INTO movies (rank, title, stars, year, duration)
+                   VALUES (%s, %s, %s, %s, %s)
+                   ON CONFLICT (rank) DO UPDATE
+                   SET title = EXCLUDED.title,
+                       stars = EXCLUDED.stars,
+                       year = EXCLUDED.year,
+                       duration = EXCLUDED.duration""",
+                values,
+            )
+
+        except IntegrityError as integrity_error:
+            spider.logger.error(f"IntegrityError: {integrity_error}")
+            self.connection.rollback()
+
+        except DataError as data_error:
+            spider.logger.error(f"DataError: {data_error}")
+            self.connection.rollback()
+
+        except OperationalError as operational_error:
+            spider.logger.error(f"OperationalError: {operational_error}")
+            self.connection.rollback()
+
+    def close_spider(self, spider):
+        try:
+            self.store_db(self.items_to_store, spider)
+        except Exception as e:
+            spider.logger.error(f"Error closing spider: {e}")
+
+        self.close_connection()
+        return self.items_to_store
