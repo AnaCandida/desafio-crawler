6110c18ae6329034d17fda7e7a412e5a291a50c8
AnaCandida <ana.candida@instruct.com.br>
2023-11-14 13:31:50 -0300

    feat(pandas): integrate Pandas DataFrame display in the run_spider script
    Integrates Pandas to display data from the database in DataFrame format within the run_spider.py script.
    If no data is present, it logs an informative message.

--- imdb_crawler/run_spider.py
+++ imdb_crawler/run_spider.py
@@ -1,4 +1,4 @@
-# run_spider.py
+#!/usr/bin/env python
 import sys
 import time
 from multiprocessing.context import Process
@@ -10,9 +10,15 @@ from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 
 from imdb_crawler.spiders.imdb_spider import ImdbSpiderSpider
+from pandas_dataframe import query_db_and_create_dataframe
 
 
-
+def get_dataframe():
+    df_database = query_db_and_create_dataframe()
+    if df_database.empty:
+        logger.info("No data")
+    else:
+        logger.info(f"\n Dados do banco de dados: \n {df_database}")
 
 
 def run_spider():
@@ -26,18 +32,20 @@ def run_process():
     process.start()
     process.join()
 
+    get_dataframe()
 
 
 if __name__ == "__main__":
-    logger.info("Rodando spider isoladamente")
-    run_process()
-else:
-    logger.info("Agendamento de spider realizado.")
-    # schedule.every(SPIDER_FREQUENCY).minutes.do(run_process)
-    schedule.every().day.at("10:50").do(run_process)
-    # schedule.every().tuesday.at("14:00").do(run_process)
+    if len(sys.argv) > 0 and sys.argv[1] == "run_once":
+        logger.info("Rodando spider isoladamente")
+        run_process()
+    else:
+        logger.info("Agendamento de spider realizado.")
+        # schedule.every(SPIDER_FREQUENCY).minutes.do(run_process)
+        schedule.every().day.at("14:50").do(run_process)
+        # schedule.every().tuesday.at("14:00").do(run_process)
 
-    # Manter o script em execução para que o agendamento funcione
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
+        # Manter o script em execução para que o agendamento funcione
+        while True:
+            schedule.run_pending()
+            time.sleep(1)
