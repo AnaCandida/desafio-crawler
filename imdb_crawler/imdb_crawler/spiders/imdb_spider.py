1cb78b3d2b09bb1b2b33a3e2cc3b3dfb30711986
AnaCandida <ana.candida@instruct.com.br>
2023-11-15 15:47:34 -0300

    refactor(spider): integrate Playwright into IMDb spider
    This refactor introduces Playwright integration into the IMDb spider code, incorporating the following changes:
    
    - Addition of Playwright settings in the spider class
    - an asynchronous method to capture a screenshot as proof of execution
    - asynchronous browser interactions, leading to more efficient data parsing
    - a custom function, should_abort_request, to control which requests Playwright should abort, thereby enhancing performance.

--- imdb_crawler/imdb_crawler/spiders/imdb_spider.py
+++ imdb_crawler/imdb_crawler/spiders/imdb_spider.py
@@ -1,9 +1,22 @@
 import logging
 
 import scrapy
+from scrapy_playwright.page import PageMethod
 
 from imdb_crawler.items import ImdbCrawlerItem
 from imdb_crawler.itemsloaders import ImdbCrawlerLoader
+
+
+def should_abort_request(request):
+    if request.resource_type in [
+        "image"
+    ]:  # Se incluir "script", "stylesheet", "font" aumentaria a performance, mas impacta na exibição dos elementos pro screenshot
+        return True
+
+    if request.method.lower() == "post":
+        return True
+
+    return False
 
 
 class ImdbSpiderSpider(scrapy.Spider):
@@ -29,27 +42,59 @@ class ImdbSpiderSpider(scrapy.Spider):
     allowed_domains = ["imdb.com"]
     start_urls = ["https://www.imdb.com/chart/top/?ref_=nv_mv_250"]
 
-    def parse(self, response):
+    custom_settings = {
+        "CONCURRENT_REQUESTS": 8,
+        "PLAYWRIGHT_ABORT_REQUEST": should_abort_request,
+    }
+
+    def start_requests(self):
+        for url in self.start_urls:
+            yield scrapy.Request(
+                url=url,
+                callback=self.parse,
+                meta={
+                    "playwright": True,
+                    "playwright_include_page": True,
+                    "playwright_page_methods": [
+                        PageMethod(
+                            "wait_for_selector", "div.ipc-metadata-list-summary-item__c"
+                        )
+                    ],
+                },
+            )
+
+    async def capture_screenshot(self, page, filename):
+        await page.screenshot(path=filename, type="jpeg", quality=30)
+
+    async def parse(self, response):
         self.logger.info("Starting data parsing")
-        DIV_MOVIE_SELECTOR = "div.ipc-metadata-list-summary-item__c"
+        MOVIE_SELECTOR = "div.ipc-metadata-list-summary-item__c"
         TITLE_SELECTOR = ".ipc-title__text::text"
-        SUMARY_SELECTOR = "span.cli-title-metadata-item"
+        SUMMARY_SELECTOR = "span.cli-title-metadata-item"
         STARS_SELECTOR = "span.ipc-rating-star--imdb::text"
 
-        divs_movies = response.css(DIV_MOVIE_SELECTOR)
-
-        for div_movie in divs_movies:
-            movie_loader = ImdbCrawlerLoader(item=ImdbCrawlerItem(), selector=div_movie)
-            title_text = div_movie.css(TITLE_SELECTOR).get()
+        movies = response.css(MOVIE_SELECTOR)
+        for movie in movies:
+            title_text = movie.css(TITLE_SELECTOR).get()
             rank, title = title_text.split(".", 1)
+            stars = movie.css(STARS_SELECTOR).get()
+            summary_info = movie.css(SUMMARY_SELECTOR)
+            year = summary_info.css(":nth-child(1)::text").get()
+            duration = summary_info.css(":nth-child(2)::text").get()
+
+            movie_loader = ImdbCrawlerLoader(item=ImdbCrawlerItem(), selector=movie)
             movie_loader.add_value("rank", rank)
             movie_loader.add_value("title", title)
-            movie_loader.add_css("stars", STARS_SELECTOR)
-            movie_loader.add_css("year", f"{SUMARY_SELECTOR}:nth-child(1)::text")
-            movie_loader.add_css("duration", f"{SUMARY_SELECTOR}:nth-child(2)::text")
-            movie_loader.add_value("url", response.url)
+            movie_loader.add_value("stars", stars)
+            movie_loader.add_value("year", year)
+            movie_loader.add_value("duration", duration)
 
             yield movie_loader.load_item()
+
+        await self.capture_screenshot(
+            page=response.meta["playwright_page"],
+            filename="execution_proof/execution_proof.jpeg",
+        )
 
         self.logger.info("Parsing finished")
 
