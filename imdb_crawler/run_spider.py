44272bbf8f942db749bb78bd9bb3f9cde1a1c52b
AnaCandida <ana.candida@instruct.com.br>
2023-11-15 19:06:01 -0300

    refactor(spider): refactor execution script for scheduled runs
    - Modified the spider execution script to include scheduling functionality.
    - Users can now specify the day and time for automatic spider runs.
    - Improved the code to support scheduling features.

--- imdb_crawler/run_spider.py
+++ imdb_crawler/run_spider.py
@@ -1,16 +1,12 @@
-#!/usr/bin/env python
-import sys
 import time
 from multiprocessing.context import Process
 
-import pandas as pd
-import schedule
 from loguru import logger
+from pandas_dataframe import query_db_and_create_dataframe
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 
 from imdb_crawler.spiders.imdb_spider import ImdbSpiderSpider
-from pandas_dataframe import query_db_and_create_dataframe
 
 
 def get_dataframe():
@@ -18,7 +14,7 @@ def get_dataframe():
     if df_database.empty:
         logger.info("No data")
     else:
-        logger.info(f"\n Dados do banco de dados: \n {df_database}")
+        logger.info(f"\n Database data: \n {df_database}")
 
 
 def run_spider():
@@ -27,25 +23,8 @@ def run_spider():
     crawler.start()
 
 
-def run_process():
+def execute_spider():
     process = Process(target=run_spider)
     process.start()
     process.join()
-
     get_dataframe()
-
-
-if __name__ == "__main__":
-    if len(sys.argv) > 0 and sys.argv[1] == "run_once":
-        logger.info("Rodando spider isoladamente")
-        run_process()
-    else:
-        logger.info("Agendamento de spider realizado.")
-        # schedule.every(SPIDER_FREQUENCY).minutes.do(run_process)
-        schedule.every().day.at("14:50").do(run_process)
-        # schedule.every().tuesday.at("14:00").do(run_process)
-
-        # Manter o script em execução para que o agendamento funcione
-        while True:
-            schedule.run_pending()
-            time.sleep(1)
